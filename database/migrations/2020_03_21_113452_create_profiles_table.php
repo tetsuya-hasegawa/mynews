9ff6973cd0f2062a7922f9117b6586a96fea0409
tetsuya-hasegawa <hase1123v@gmail.com>
2020-03-22 07:32:54 +0000

    Laravel14

---
+++ database/migrations/2020_03_21_113452_create_profiles_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProfilesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name'); // 名前
+            $table->string('gender'); // 性別
+            $table->string('hobby'); // 趣味
+            $table->string('introduction'); // 自己紹介
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('profiles');
+    }
+}
